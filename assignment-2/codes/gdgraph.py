ba6dc080c0b1823307721c90f14ad116dd59ad94
Hema-Sri-Ch <102508770+Hema-Sri-Ch@users.noreply.github.com>
2022-04-20 13:59:27 +0530

    Add files via upload

--- assignment-2/codes/gdgraph.py
+++ assignment-2/codes/gdgraph.py
@@ -9,20 +9,31 @@ import matplotlib.pyplot as plt
 import sympy as sp
 
 
+# f(x) --> Marginal cost function
 def f(x):
 	return x*x - 90*x - 900
+	
 
+# f'(x)
 def dy_dx(x):
 	return 2*(x-45)
 
+# Points to be plotted
 xcoords = list()
 ycoords = list()
 
+# Curve to be plotted
 xpts = np.linspace(-10, 100, 1000)
 ypts = f(xpts)
-# x_k = x_k - (0.25 * MC.subs(x, x_k))
+
+
+# VERIFYING AND PLOTTING
+
+# x_k = x_k - (0.05 * f'(x_k))
+# initial assumption of x_k as zero
 x_k =  0
 
+# Iterations till the slope approaches zero
 while dy_dx(x_k) < -0.000001 :
 #	print(x_k)
 	xcoords.append(x_k)
@@ -30,9 +41,11 @@ while dy_dx(x_k) < -0.000001 :
 	x_k = x_k - (0.05 * dy_dx(x_k))
 	
 print("The value of x at where the minimum for this function exists is nearly ",x_k)
-# The Possible number of units to be produced for minimum marginal cost is the nearest positive integer that its tending to 
+# X --> The Possible number of units to be produced for minimum marginal cost is the nearest positive integer that its tending to 
 X = round(x_k)
 print("Therefore, the number of units that are to be produced for minimum marginal cost is ", X)
+
+# PLOTTING
 
 plt.scatter(X, 0, color = "r")
 plt.plot(xpts, ypts, color = "blue", label = "Marginal cost function")
